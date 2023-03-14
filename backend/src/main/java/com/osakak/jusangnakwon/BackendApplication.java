47887057e1fcb7e24ae4606f175e61ad7bab9b76
nowgnas <dev.nowgnas@gmail.com>
2023-03-14 13:45:55 +0900

    :art: chore: 백엔드 환경설정 완료 및 기본 코드 추가

---
+++ backend/src/main/java/com/osakak/jusangnakwon/BackendApplication.java
@@ -0,0 +1,16 @@
+package com.osakak.jusangnakwon;
+
+import com.osakak.jusangnakwon.common.config.EnvConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+
+@SpringBootApplication
+@PropertySource(value = {"classpath:environment/env.yml"}, factory = EnvConfig.class)
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
