095e4523e922ca5c03a36958e20a1f9609ed0ab1
nowgnas <dev.nowgnas@gmail.com>
2023-03-15 16:34:03 +0900

    :art: chore: add swagger config
    - swagger 설정 추가 \n- application.yml 변경

--- backend/src/main/java/com/osakak/jusangnakwon/BackendApplication.java
+++ backend/src/main/java/com/osakak/jusangnakwon/BackendApplication.java
@@ -3,9 +3,11 @@ package com.osakak.jusangnakwon;
 import com.osakak.jusangnakwon.common.config.EnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy
 @PropertySource(value = {"classpath:environment/env.yml"}, factory = EnvConfig.class)
 public class BackendApplication {
 
