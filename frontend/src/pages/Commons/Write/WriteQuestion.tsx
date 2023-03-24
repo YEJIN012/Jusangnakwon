f0fb23e5367bff0a2e66bb8416a3311aa6d1646c
이예진 <yjlee66096@gmail.com>
2023-03-24 17:51:29 +0900

    :sparkles: feat: 게시글, 질문글 작성페이지

--- frontend/src/pages/Commons/Write/WriteQuestion.tsx
+++ frontend/src/pages/Commons/Write/WriteQuestion.tsx
@@ -1,24 +1,46 @@
-import styles from "./Write.module.css";
-import PhotoCameraIcon from "@mui/icons-material/PhotoCamera";
-import { Rating, Select } from "@mui/material";
-import Search from "@mui/icons-material/Search";
 import { useState } from "react";
-import MenuItem from "@mui/material/MenuItem";
+import { useNavigate } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
+import { pink } from "@mui/material/colors";
+import { alpha, styled } from "@mui/material/styles";
+import { Rating } from "@mui/material";
 import Modal from "@mui/joy/Modal";
 import ModalDialog from "@mui/joy/ModalDialog";
-import { useNavigate } from "react-router-dom";
+import Switch from "@mui/material/Switch";
 import CloseIcon from "@mui/icons-material/Close";
+import Search from "@mui/icons-material/Search";
+import LockIcon from "@mui/icons-material/Lock";
+import LockOpenIcon from "@mui/icons-material/LockOpen";
+import StarIcon from "@mui/icons-material/Star";
+import styles from "./Write.module.css";
+import ImageUpload from "@/components/Commons/ImageUpload/ImageUpload";
 
 interface FormData {
-  img: string;
-  type: string;
-  name: string;
-  content: string;
-  ratings: number;
-  isPrivate: boolean;
+  img: string | null;
+  title: string;
+  content: string | null;
+  // isPrivate: boolean;
 }
 
-// 임의리스트
+// const StyleModal = styled(ModalDialog)(({theme}) => ({
+//     "& .JoyModal-backdrop": {
+//       backgroundColor:" #000000"
+//     },
+// }));
+
+const StyleSwitch = styled(Switch)(({ theme }) => ({
+  "& .MuiSwitch-switchBase.Mui-checked": {
+    color: pink[600],
+    "&:hover": {
+      backgroundColor: alpha(pink[600], theme.palette.action.hoverOpacity),
+    },
+  },
+  "& .MuiSwitch-switchBase.Mui-checked + .MuiSwitch-track": {
+    backgroundColor: pink[600],
+  },
+}));
+
+// 임의 리스트
 const DrinkTypeList = {
   와인: "l1",
   전통주: "l2",
@@ -29,127 +51,67 @@ const DrinkTypeList = {
 };
 
 const WriteQuestion = () => {
-  const [type, setType] = useState("");
-  const [name, setName] = useState("");
-  const [content, setContent] = useState("");
-  const [ratings, setRatings] = useState(0);
-  const [isPrivate, setPrivate] = useState(false);
-
   const [formData, setFormData] = useState({
     img: "",
-    type: type,
-    name: name,
-    content: content,
-    ratings: ratings,
-    isPrivate: isPrivate,
+    title: "",
+    content: "",
+    // isPrivate: false,
   });
 
-  const [open, setOpen] = useState(false);
 
   const navigate = useNavigate();
+
+  const handleSubmit = (formData: FormData) => {
+    // 제출 api호출
+    navigate(-1);
+  };
+
   const WriteHeader = () => {
     return (
       <div className={`${styles[`header-container`]}`}>
         <CloseIcon onClick={() => navigate(-1)} />
-        <div>게시글 작성</div>
-        <div>완료</div>
+        <div>질문글 작성</div>
+        <div onClick={() => handleSubmit(formData)}>완료</div>
       </div>
     );
   };
 
-  const handleRatingChange = (event: React.ChangeEvent<{}>, newValue: number | null) => {
-    if (newValue !== null) {
-      setRatings(newValue);
-    }
-  };
-
-  const handleSubmit = (formData: FormData) => {};
-
+  
   return (
     <div className={`${styles[`container`]}`}>
       <WriteHeader></WriteHeader>
-      <form
-        className={`${styles[`container`]}`}
-        onSubmit={() => {
-          handleSubmit(formData);
-        }}
-      >
+      <form className={`${styles[`container`]}`}>
         <div className={`${styles[`row-container`]}`}>
-          <div>
+          <div style={{width:"inherit"}}>
             <div className={`${styles[`subtitle-row`]}`}>
               사진
               <div style={{ fontSize: "0.8rem", color: "rgb(149, 149, 149)" }}> (선택)</div>
             </div>
-            <div className={`${styles[`camera-box`]}`}>
-              <PhotoCameraIcon sx={{ fontSize: { xs: 25, sm: 35, md: 40, lg: 50 } }} />
-            </div>
+            {/* 이미지 선택, 미리보기, 업로드 로직 컴포넌트 */}
+            <ImageUpload></ImageUpload>
           </div>
         </div>
 
         <div className={`${styles[`row-container`]}`}>
-          {/* 술상세페이지에서 리뷰작성으로 넘어오면 */}
-          {/* navigate state로 주종 같이 넘겨줘서 select value에 담아놓기  */}
-          <div className={`${styles[`subtitle-container`]}`}>주종</div>
-          <Select
-            style={{ color: "red" }}
-            value={type}
-            onChange={(e) => {
-              setType(e.target.value);
-            }}
-          >
-            {Object.keys(DrinkTypeList).map((type, index) => (
-              <MenuItem key={index} value={type}>
-                {type}
-              </MenuItem>
-            ))}
-          </Select>
-        </div>
-
-        <div className={`${styles[`row-container`]}`}>
-          <div className={`${styles[`subtitle-container`]}`}>술이름</div>
-          <div className={`${styles[`end-container`]}`}>
-            <Search onClick={() => setOpen(true)} />
-          </div>
-        </div>
-
-        <div className={`${styles[`row-container`]}`}>
-          <textarea
-            style={{
-              backgroundColor: "rgba(149, 149, 149, 0)",
-              width: "100%",
-              height: "20vh",
-              border: "solid rgba(149, 149, 149, 0)",
-            }}
-            placeholder="내용 입력..."
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+          <div className={`${styles[`subtitle-container`]}`}>글 제목</div>
+          <input
+            value={formData.title}
+            onChange={(e) => setFormData({ ...formData, title: e.target.value })}
           />
         </div>
 
         <div className={`${styles[`row-container`]}`}>
-          <div className={`${styles[`subtitle-container`]}`}>별점</div>
-          <Rating value={ratings} onChange={handleRatingChange} />
-        </div>
-
-        <div className={`${styles[`row-container`]}`}>
-          <div className={`${styles[`subtitle-container`]}`}>공개여부</div>
-          <button onClick={() => setPrivate(false)}>공개</button>
-          <button onClick={() => setPrivate(true)}>비공개</button>
+          <textarea
+            placeholder="내용 입력..."
+            value={formData.content}
+            onChange={(e) => setFormData({ ...formData, content: e.target.value })}
+          />
         </div>
       </form>
-
-      <Modal open={open} onClose={() => setOpen(false)}>
-        <ModalDialog color="neutral" variant="plain">
-          {/* <ModalClose /> */}술 이름 검색
-        </ModalDialog>
-      </Modal>
-
       <div>
-        {type}
-        {name}
-        {content}
-        {ratings}
-        {isPrivate}
+        데이터 확인 :
+        {formData.title}
+        {formData.content}
       </div>
     </div>
   );
