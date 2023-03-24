f0a7a3566d1a83a19361f0299610ba8746fa709c
irang6v6 <111601joo@naver.com>
2023-03-24 10:19:35 +0900

    :card_file_box: feat: MongoDB to MySQL

--- backend/src/main/java/com/osakak/jusangnakwon/domain/user/dao/UserRepository.java
+++ backend/src/main/java/com/osakak/jusangnakwon/domain/user/dao/UserRepository.java
@@ -2,10 +2,8 @@ package com.osakak.jusangnakwon.domain.user.dao;
 
 import com.osakak.jusangnakwon.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserId(String userId);
+    public User findByUserId(String userId);
 }
 
--- backend/src/main/java/com/osakak/jusangnakwon/domain/user/entity/User.java
+++ backend/src/main/java/com/osakak/jusangnakwon/domain/user/entity/User.java
@@ -1,30 +1,25 @@
 package com.osakak.jusangnakwon.domain.user.entity;
 
-import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 import com.osakak.jusangnakwon.common.oauth.entity.ProviderType;
 import com.osakak.jusangnakwon.common.oauth.entity.RoleType;
 import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.hibernate.annotations.CreationTimestamp;
-import org.springframework.data.annotation.Id;
 
+import javax.persistence.*;
 import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
-@Getter
-@Setter
 @Entity
+@ToString
+@Getter
+@NoArgsConstructor
 @Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     @Column(name = "USER_ID", nullable = false, length = 64, unique = true)
     private String userId;
@@ -35,20 +30,22 @@ public class User {
     @Column(name = "is_deleted", nullable = false)
     private boolean isDeleted;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "provider", nullable = false, length = 10)
     private ProviderType providerType;
 
-    @Column(name = "email", nullable = false, length = 50)
+    @Column(name = "email", length = 50)
     private String email;
 
     @CreationTimestamp
     @Column(name = "date_registered")
     private Timestamp dateRegisted;
 
+    @Enumerated(EnumType.STRING)
     @Column(name = "role", nullable = false, length = 50)
     private RoleType role;
 
-    @Column(name = "role", nullable = false, length = 512)
+    @Column(name = "profile_img", nullable = false, length = 512)
     private String profileImageUrl;
 
     @Builder
@@ -63,7 +60,4 @@ public class User {
         this.profileImageUrl = profileImageUrl;
     }
 
-    public User() {
-
-    }
 }
