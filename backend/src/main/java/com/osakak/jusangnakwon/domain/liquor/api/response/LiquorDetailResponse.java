738eb3860b2f21cfa44381949d9c6e6b72db1c57
양주연 <gami7477@naver.com>
2023-03-31 21:42:27 +0900

    :alien: update: LiquorDetailResponse 필드 수정

--- backend/src/main/java/com/osakak/jusangnakwon/domain/liquor/api/response/LiquorDetailResponse.java
+++ backend/src/main/java/com/osakak/jusangnakwon/domain/liquor/api/response/LiquorDetailResponse.java
@@ -1,7 +1,9 @@
 package com.osakak.jusangnakwon.domain.liquor.api.response;
 
+import com.osakak.jusangnakwon.domain.feed.dto.WriterDto;
 import com.osakak.jusangnakwon.domain.feed.entity.Feed;
 import com.osakak.jusangnakwon.domain.liquor.dto.LiquorListItemDto;
+import com.osakak.jusangnakwon.domain.liquor.dto.ReviewListDto;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -11,29 +13,34 @@ import java.util.List;
 public class LiquorDetailResponse {
     private final Long id;
     private final String name;
+    private final WriterDto writer;
+    private final String image;
     private final Double ratingAvg;
     private final Long scrapCnt;
     private final Boolean scrapped;
-    private final String description;
-    private final List<Feed> feeds;
+    private final List<String> ingredients;
     private final List<String> tastes;
-    private final List<LiquorListItemDto> similarItem;
-    private final String image;
+    private final String description;
+    private final List<ReviewListDto> reviews;
+    private final List<LiquorListItemDto> similarItems;
+
 
     @Builder
-    public LiquorDetailResponse(Long id, String name, Double ratingAvg, Long scrapCnt,
-            Boolean scrapped,
-            String description, List<Feed> feeds, List<String> tastes,
-            List<LiquorListItemDto> similarItem, String image) {
+    public LiquorDetailResponse(Long id, String name, WriterDto writer, String image,
+            Double ratingAvg,
+            Long scrapCnt, Boolean scrapped, List<String> ingredients, List<String> tastes,
+            String description, List<ReviewListDto> reviews, List<LiquorListItemDto> similarItems) {
         this.id = id;
         this.name = name;
+        this.writer = writer;
+        this.image = image;
         this.ratingAvg = ratingAvg;
         this.scrapCnt = scrapCnt;
         this.scrapped = scrapped;
-        this.description = description;
-        this.feeds = feeds;
+        this.ingredients = ingredients;
         this.tastes = tastes;
-        this.similarItem = similarItem;
-        this.image = image;
+        this.description = description;
+        this.reviews = reviews;
+        this.similarItems = similarItems;
     }
 }
