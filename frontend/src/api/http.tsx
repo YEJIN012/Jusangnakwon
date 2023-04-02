847edeed7eddb25c8dd0af0f2992923dc8bf0340
이예진 <yjlee66096@gmail.com>
2023-04-02 17:47:21 +0900

    :sparkles: chore: main test를 위한 주석풀기

--- frontend/src/api/http.tsx
+++ frontend/src/api/http.tsx
@@ -14,44 +14,44 @@ const getApiInstance = () => {
     },
   });
 
-  // instance.interceptors.response.use(
-  //   (response: AxiosResponse) => {
-  //     console.log("interceptor response 200");
-  //     return response;
-  //   },
+  instance.interceptors.response.use(
+    (response: AxiosResponse) => {
+      console.log("interceptor response 200");
+      return response;
+    },
 
-  //   async (error) => {
-  //     const {
-  //       config,
-  //       response: { status },
-  //     } = error;
+    async (error) => {
+      const {
+        config,
+        response: { status },
+      } = error;
 
-  //     // statusCode 402 : 만료된 토큰
-  //     if (error.status === 402) {
-  //       console.log(error.message);
-  //       // const [cookies, setCookie] = useCookies(["refreshToken"]);
-  //       const originalRequest = config;
-  //       // const refreshToken = cookies;
+      // statusCode 402 : 만료된 토큰
+      if (error.status === 402) {
+        console.log(error.message);
+        // const [cookies, setCookie] = useCookies(["refreshToken"]);
+        const originalRequest = config;
+        // const refreshToken = cookies;
 
-  //       // 토큰 재발급을 위한 요청
-  //       refreshAccessToken().then((r) => {
-  //         console.log(r);
+        // 토큰 재발급을 위한 요청
+        refreshAccessToken().then((r) => {
+          console.log(r);
 
-  //         const accessToken = r?.data.body;
+          const accessToken = r?.data.body;
 
-  //         // 재발급된 토큰을 기존요청에 다시 담아서 ->
-  //         originalRequest.headers.Authorization = `Bearer ${accessToken}`;
-  //         // axios 디폴트값에도 갱신해준다.
-  //         axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
-  //         // -> 재요청
-  //         return axios(originalRequest);
-  //       });
-  //     }
-  //     console.log("refreshToken 재발급 : ", error);
-  //     redirect("/login");
-  //     return Promise.reject(error);
-  //   },
-  // );
+          // 재발급된 토큰을 기존요청에 다시 담아서 ->
+          originalRequest.headers.Authorization = `Bearer ${accessToken}`;
+          // axios 디폴트값에도 갱신해준다.
+          axios.defaults.headers.common["Authorization"] = `Bearer ${accessToken}`;
+          // -> 재요청
+          return axios(originalRequest);
+        });
+      }
+      console.log("refreshToken 재발급 : ", error);
+      redirect("/login");
+      return Promise.reject(error);
+    },
+  );
   return instance;
 };
 
