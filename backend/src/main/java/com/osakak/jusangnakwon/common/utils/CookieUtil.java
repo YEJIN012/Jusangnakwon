253be3fef2195f18995deb0822cdd12fcfe3d837
irang6v6 <111601joo@naver.com>
2023-04-03 16:42:45 +0900

    :bug: fix: cookie - http only option false

--- backend/src/main/java/com/osakak/jusangnakwon/common/utils/CookieUtil.java
+++ backend/src/main/java/com/osakak/jusangnakwon/common/utils/CookieUtil.java
@@ -32,6 +32,15 @@ public class CookieUtil {
         response.addCookie(cookie);
     }
 
+    public static void addCookieTest(HttpServletResponse response, String name, String value, int maxAge) {
+        Cookie cookie = new Cookie(name, value);
+        cookie.setPath("/");
+        cookie.setHttpOnly(false);
+        cookie.setMaxAge(maxAge);
+
+        response.addCookie(cookie);
+    }
+
     public static void deleteCookie(HttpServletRequest request, HttpServletResponse response, String name) {
         Cookie[] cookies = request.getCookies();
 
