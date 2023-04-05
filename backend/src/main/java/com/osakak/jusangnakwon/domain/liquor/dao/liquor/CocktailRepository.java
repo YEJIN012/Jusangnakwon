f0469b8a1991a937bd108cb55b4311667fc60393
nowgnas <dev.nowgnas@gmail.com>
2023-04-05 17:54:49 +0900

    :bug: fix: amend sort option

--- backend/src/main/java/com/osakak/jusangnakwon/domain/liquor/dao/liquor/CocktailRepository.java
+++ backend/src/main/java/com/osakak/jusangnakwon/domain/liquor/dao/liquor/CocktailRepository.java
@@ -1,5 +1,6 @@
 package com.osakak.jusangnakwon.domain.liquor.dao.liquor;
 
+import com.osakak.jusangnakwon.domain.liquor.dto.LiquorListItemDto;
 import com.osakak.jusangnakwon.domain.liquor.entity.liquor.Beer;
 import com.osakak.jusangnakwon.domain.liquor.entity.liquor.Cocktail;
 import com.osakak.jusangnakwon.domain.liquor.entity.liquor.Whisky;
@@ -20,8 +21,10 @@ public interface CocktailRepository extends JpaRepository<Cocktail, Long>,Cockta
      * @param pageable 페이징 정보
      * @return 페이징 포함 칵테일 리스트
      */
-    @Query("select c from Cocktail c order by c.ratingAvg desc")
-    Page<Cocktail> findByRatingAvg(Pageable pageable);
+    @Query("select new com.osakak.jusangnakwon.domain.liquor.dto.LiquorListItemDto(l.id, l.name, l.img, l.liquorType) " +
+            "from Cocktail l " +
+            "order by l.ratingAvg desc, l.name")
+    Page<LiquorListItemDto> findListByRatingIsNotLoggedIn(Pageable pageable);
 
     @Query("select l from Cocktail l where l.name like :keyword%")
     Optional<List<Cocktail>> findByKeyword(@Param("keyword") String keyword);
@@ -31,5 +34,14 @@ public interface CocktailRepository extends JpaRepository<Cocktail, Long>,Cockta
 
     @Query("select l from  Cocktail l where l.id in (:id)")
     List<Cocktail> findByIdList(@Param("id")List<Long> id);
+
+    @Query("select new com.osakak.jusangnakwon.domain.liquor.dto.LiquorListItemDto(l.id, l.name, l.img, l.liquorType, s.scrapped) " +
+            "from Cocktail l " +
+            "left join fetch Scrap s " +
+            "on l.liquorType=s.liquorType and l.id=s.liquorId " +
+            "left join fetch User u " +
+            "on u.id=s.user.id " +
+            "order by l.ratingAvg desc, l.name ")
+    Page<LiquorListItemDto> findListByRatingIsLogin(Pageable pageable);
 }
 
--- backend/src/main/java/com/osakak/jusangnakwon/domain/liquor/dao/liquor/TraditionRepository.java
+++ backend/src/main/java/com/osakak/jusangnakwon/domain/liquor/dao/liquor/TraditionRepository.java
@@ -1,5 +1,6 @@
 package com.osakak.jusangnakwon.domain.liquor.dao.liquor;
 
+import com.osakak.jusangnakwon.domain.liquor.dto.LiquorListItemDto;
 import com.osakak.jusangnakwon.domain.liquor.entity.liquor.Cocktail;
 import com.osakak.jusangnakwon.domain.liquor.entity.liquor.Tradition;
 import org.springframework.data.domain.Page;
@@ -19,8 +20,10 @@ public interface TraditionRepository extends JpaRepository<Tradition, Long>,Trad
      * @param pageable 페이징 정보
      * @return 페이징 포함 전통주 칵테일 리스트
      */
-    @Query("select c from Tradition c order by c.ratingAvg desc")
-    Page<Tradition> findByRatingAvg(Pageable pageable);
+    @Query("select new com.osakak.jusangnakwon.domain.liquor.dto.LiquorListItemDto(l.id, l.name, l.img, l.liquorType) " +
+            "from Tradition l " +
+            "order by l.ratingAvg desc, l.name ")
+    Page<LiquorListItemDto> findListByRatingIsNotLoggedIn(Pageable pageable);
 
     @Query("select l from Tradition l where l.name like :keyword%")
     Optional<List<Tradition>> findByKeyword(@Param("keyword") String keyword);
@@ -30,5 +33,14 @@ public interface TraditionRepository extends JpaRepository<Tradition, Long>,Trad
 
     @Query("select l from  Tradition l where l.id in (:id)")
     List<Tradition> findByIdList(@Param("id")List<Long> id);
+
+    @Query("select new com.osakak.jusangnakwon.domain.liquor.dto.LiquorListItemDto(l.id, l.name, l.img, l.liquorType, s.scrapped) " +
+            "from Tradition l " +
+            "left join fetch Scrap s " +
+            "on l.liquorType=s.liquorType and l.id=s.liquorId " +
+            "left join fetch User u " +
+            "on u.id=s.user.id " +
+            "order by l.ratingAvg desc, l.name ")
+    Page<LiquorListItemDto> findListByRatingIsLogin(Pageable pageable);
 }
 
